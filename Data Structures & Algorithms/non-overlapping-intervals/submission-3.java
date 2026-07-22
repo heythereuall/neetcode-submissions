0238b6402d2d9ea057d57c4d80261dcaf4d590ec
Aditi Jaiswal <95347598+heythereuall@users.noreply.github.com>
2026-07-22 20:44:54 +0530

    Add: non-overlapping-intervals - submission-3

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-3.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+        int n = intervals.length;
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (intervals[j][1] <= intervals[i][0]) {
+                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+                }
+            }
+        }
+
+        int maxNonOverlapping = Arrays.stream(dp).max().getAsInt();
+        return n - maxNonOverlapping;
+    }
+}
