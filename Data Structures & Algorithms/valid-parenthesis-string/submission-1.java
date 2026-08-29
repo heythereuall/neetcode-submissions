79932fa76cba59ecb63b3e03d9e3cc9e9b9f5a5b
Aditi Jaiswal <95347598+heythereuall@users.noreply.github.com>
2026-08-30 00:55:45 +0530

    Add: valid-parenthesis-string - submission-1

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-1.java
@@ -0,0 +1,26 @@
+public class Solution {
+        public boolean checkValidString(String s) {
+                int n = s.length();
+                        boolean[][] dp = new boolean[n + 1][n + 1];
+                                dp[n][0] = true;
+
+                                        for (int i = n - 1; i >= 0; i--) {
+                                                    for (int open = 0; open < n; open++) {
+                                                                    boolean res = false;
+                                                                                    if (s.charAt(i) == '*') {
+                                                                                                        res |= dp[i + 1][open + 1];
+                                                                                                                            if (open > 0) res |= dp[i + 1][open - 1];
+                                                                                                                                                res |= dp[i + 1][open];
+                                                                                                                                                                } else {
+                                                                                                                                                                                    if (s.charAt(i) == '(') {
+                                                                                                                                                                                                            res |= dp[i + 1][open + 1];
+                                                                                                                                                                                                                                } else if (open > 0) {
+                                                                                                                                                                                                                                                        res |= dp[i + 1][open - 1];
+                                                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                                                                                            dp[i][open] = res;
+                                                                                                                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                                                                                                                        return dp[0][0];
+                                                                                                                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                                                                                                                            }
